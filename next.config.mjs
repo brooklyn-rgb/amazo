e0dc1d80733ebf6944c3a3cd7f7870aebf5052e4
brooklyn-rgb <brooklynstore151@gmail.com>
2024-09-11 13:41:26 +0200

    initial files

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+          {
+            protocol: 'http',
+            hostname: 'utfs.io',
+            port: '',
+          },
+        ],
+      },
+};
 
 export default nextConfig;
