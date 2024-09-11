defb46cf7e01c74ed25f348278afe3326d7f1950
brooklyn-rgb <brooklynstore151@gmail.com>
2024-09-11 09:10:08 +0200

    first commit

---
+++ lib/sample-data.ts
@@ -0,0 +1,64 @@
+const sampleData ={
+    products: [
+        {
+            name: 'CANON EOS 2000D with 18-55mm f/3.5-5.6 III Lens ',
+            slug: 'canon-eos-2000d',
+            category: "eos-camera",
+            images: ['/assets/images/2000D.png', '/assets/images/2000D.png'],
+            price: '5899',
+            brand: 'Canon',
+            rating: '4.5',
+            numReviews: 10,
+            stock: 9,
+            description:
+              '24.1 Megapixel CMOS (APS-C) Sensor with ISO 100-6400 (H: 12800) 3.0-inch LCD monitor DIGIC 4+ Image Processor 9-point autofocus and AI Servo AF Full HD 1080p video recording at 30 fps Built-in Wi-Fi & NFC Capability.',
+            isFeatured: true,
+            banner: '/assets/images/banner-1.jpeg',
+          },
+          {
+            name: 'CANON EOS 4000D with 18-55mm Lens ',
+            slug: 'canon-eos-4000d',
+            category: "eos-camera",
+            images: ['/assets/images/4000D.png', '/assets/images/4000D.png'],
+            price: '4599',
+            brand: 'Canon',
+            rating: '4.5',
+            numReviews: 10,
+            stock: 9,
+            description:
+              'The Canon EOS 4000D starter kit is an affordable, entry-level DSLR that combines good image quality and a wide range of practical and easy to use features. The camera features a large 18MP APS-C CMOS sensor with good low light performance, a feature guide for users new to photography, Full HD video recording, as well as Wi-Fi connectivity to enable quick media sharing.',
+            
+          },
+          {
+            name: 'Sony HXR-NX3 NXCAM Professional Camcorder',
+            slug: 'sony-HXR-NX3',
+            category: "Sony Camcorder",
+            images: ['/assets/images/SonyNX3.png', '/assets/images/SonyNX3.png'],
+            price: '32999',
+            brand: 'Sony',
+            rating: '4.5',
+            numReviews: 10,
+            stock: 9,
+            description:
+              'A multi-purpose professional camcorder offering exceptional image quality and three Full HD sensors ready to maximise your creative potential.',
+            
+          },
+          {
+            name: 'CANON EOS 77D with 18-55mm',
+            slug: 'canon-eos-77d',
+            category: "eos-camera",
+            images: ['/assets/images/c77D.png', '/assets/images/c77D.png'],
+            price: '15999',
+            brand: 'Canon',
+            rating: '4.5',
+            numReviews: 10,
+            stock: 9,
+            description:
+              'The canon 77D is a 24 megapixel Canon Machine with powerful pictures and videos resolution that gives you best quality of pictures and videos.the FlipScreen and microphone input make your shoot very easier.',
+            
+          },
+
+    ]
+}
+
+export default sampleData
