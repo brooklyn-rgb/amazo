0f4674509952dc144f22a0a0cb6aa76c5954ef94
brooklyn-rgb <brooklynstore151@gmail.com>
2024-10-08 14:11:12 +0200

    initial files

--- app/(root)/pages/banner2/page.tsx
+++ app/(root)/pages/banner2/page.tsx
@@ -7,7 +7,7 @@ export default function Banner2() {
   <div className="card-group-2 flex col-1 justify-center ">
             
   <div className="card">
-    <Image src="https://utfs.io/f/wRli2eZYNna2b7OqgK1rCgSOhtTbrpjsMV84cFxyB0KHm7ZE" className="card-img-top" alt="Brooklyn Store" width={500} height={500}/>
+    <Image src="https://utfs.io/f/wRli2eZYNna2H2Im8SCbCtZNPTS6AEDLVIvgqxukdXz18J9Q" className="card-img-top" alt="Brooklyn Store" width={500} height={500}/>
     
   </div>
   <div className="card">
