ad93f2f302b83bac6c76a80c5ca63c798ae1d94e
brooklyn-rgb <brooklynstore151@gmail.com>
2024-10-29 18:19:15 +0200

    Update page.tsx

--- app/(root)/pages/demo2/page.tsx
+++ app/(root)/pages/demo2/page.tsx
@@ -6,15 +6,15 @@ export default function Demo2() {
            
   <div className="card-group-2 flex col-1 justify-center ">          
   <div className="card">
-    <Image src="https://utfs.io/f/wRli2eZYNna2b7OqgK1rCgSOhtTbrpjsMV84cFxyB0KHm7ZE" className="card-img-top" alt="Brooklyn Store" width={500} height={500}/>
+    <Image src="https://utfs.io/f/wRli2eZYNna2b7OqgK1rCgSOhtTbrpjsMV84cFxyB0KHm7ZE" className="card-img-top" alt="LeefstylKameras" width={500} height={500}/>
     
   </div>
   <div className="card">
-    <Image src="https://utfs.io/f/wRli2eZYNna2fnwMVIjqVZCNy1lGK0tidF5m8IDQ9MXhW6cT" className="card-img-top" alt="Brooklyn Store" width={500} height={500}/>
+    <Image src="https://utfs.io/f/wRli2eZYNna2fnwMVIjqVZCNy1lGK0tidF5m8IDQ9MXhW6cT" className="card-img-top" alt="LeefstylKameras" width={500} height={500}/>
    
   </div>
   <div className="card">
-    <Image src="https://utfs.io/f/wRli2eZYNna2yPMnbGWwRd9PEjbUeMl8itxDFYBS5VHcLaAK" className="card-img-top" alt="Brooklyn Store" width={500} height={500}/>
+    <Image src="https://utfs.io/f/wRli2eZYNna2yPMnbGWwRd9PEjbUeMl8itxDFYBS5VHcLaAK" className="card-img-top" alt="LeefstylKameras" width={500} height={500}/>
     
   </div>
 </div>    
