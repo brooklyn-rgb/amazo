1ab4ebef92e6e87f8260d179268cb14086dd6797
brooklyn-rgb <brooklynstore151@gmail.com>
2024-10-29 19:59:33 +0200

    Update index.ts

--- app/admin/index.ts
+++ app/admin/index.ts
@@ -1,7 +1,7 @@
-export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'Brooklyn Store'
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'LeefstylKameras'
 export const APP_DESCRIPTION =
   process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
-  'Brooklyn Store the most reliable camera store in South Africa'
+  'LeefstylKameras the most reliable camera store in South Africa'
 
 export const SENDER_EMAIL = process.env.SENDER_EMAIL || 'onboarding@resend.dev'
 
@@ -9,7 +9,7 @@ export const PAGE_SIZE = Number(process.env.PAGE_SIZE) || 3
 
 export const PAYMENT_METHODS = process.env.PAYMENT_METHODS
   ? process.env.PAYMENT_METHODS.split(', ')
-  : ['PayPal', 'Stripe', 'CashOnDelivery']
+  : ['PayPal', 'Stripe', 'CashOnDelivery', 'Payfast']
 export const DEFAULT_PAYMENT_METHOD =
   process.env.DEFAULT_PAYMENT_METHOD || 'PayPal'
 
